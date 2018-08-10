501cf8de9723f804c69697a49526de9400c632e7
ethanhinson <ethan@bluetent.com>
2018-08-10 14:58:34 -0600

    Source code

---
+++ data/content.js
@@ -0,0 +1,104 @@
+module.exports = {
+  pageTitle: "HAUL IT ALL",
+  services: {
+    title: "OUR SERVICES",
+    description: "HAUL IT ALL OFFERS FLAT RATES FOR TRAILER DROP OFF AND PICK UP, IN ADDITION TO HOURLY HAULING SERVICES."
+  },
+  rates: {
+    title: "RATES",
+    description: "LIMITED AVAILABILITY \n ALL PRICES SUBJECT TO CHANGE"
+  },
+  callouts: [
+    {
+      title: "HOT SHOT",
+      description: "FLAT DECK & ENCLOSED INCLUDES DRIVE TIME, LOAD & UNLOAD",
+      lineItems: [
+        {
+          name: "TRUCK BED",
+          cost: 100
+        },
+        {
+          name: "40' GOOSENECK",
+          cost: 150
+        },
+        {
+          name: "24' ENCLOSED",
+          cost: 150
+        },
+      ]
+    },
+    {
+      title: "TRASH",
+      description: "$50 DROP OFF FEE\n" +
+      "DOES NOT INCLUDE DUMP FEE",
+      lineItems: [
+        {
+          name: "10 YD",
+          cost: 250
+        },
+        {
+          name: "15 YD",
+          cost: 400
+        },
+        {
+          name: "20 YD",
+          cost: 500
+        }
+      ]
+    },
+    {
+      title: "STORAGE RENTALS\n" +
+      "24’ ENCLOSED",
+      description: "FOR STATIONARY USE ONLY\n" +
+      "$50 DROP OFF FEE",
+      lineItems: [
+        {
+          name: "1 DAY",
+          cost: 50
+        },
+        {
+          name: "1 WK",
+          cost: 100
+        },
+        {
+          name: "1 MO",
+          cost: 300
+        }
+      ]
+    },
+    {
+      title: "SNOW",
+      description: "$50 DROP OFF FEE IF APPLICABLE",
+      lineItems: [
+        {
+          name: "10 YD",
+          cost: 200
+        },
+        {
+          name: "15 YD",
+          cost: 250
+        },
+        {
+          name: "20 YD",
+          cost: 350
+        }
+      ]
+    },
+    {
+      title: "DIRT, SAND, TOP-\n" +
+      "SOIL TRUCKING",
+      description: "$50 DROP OFF FEE IF APPLICABLE\n" +
+      "DOES NOT INCLUDE MATERIAL",
+      lineItems: [
+        {
+          name: "5 YD | 10K LBS",
+          cost: "100/HR"
+        },
+        {
+          name: "10 YD | 18K LBS",
+          cost: "150/HR"
+        }
+      ]
+    }
+  ]
+};
